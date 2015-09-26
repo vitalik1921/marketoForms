93d9e30eadaf3f6a99cb8832225cbe9c91feb00e
vitalik1921 <vitalik1921@users.noreply.github.com>
2015-09-26 15:04:13 +0300

    Refactoring

--- pbl/MF_Public.php
+++ pbl/MF_Public.php
@@ -18,8 +18,8 @@ class MF_Public extends \awis_marketo_forms\inc\Plugin_Public
      */
     function add_enqueue_scripts()
     {
-        wp_enqueue_style('marketo-forms', MF_Loader::getPluginUrl() . '/pbl/css/mf_plugin.css');
-        wp_enqueue_script('marketo-forms', MF_Loader::getPluginUrl() . '/pbl/js/mf_plugin.js', array('jquery'));
+        wp_enqueue_style('marketo-forms', MF_Loader::getPluginUrl() . 'pbl/css/mf_plugin.css');
+        wp_enqueue_script('marketo-forms', MF_Loader::getPluginUrl() . 'pbl/js/mf_plugin.js', array('jquery'));
         wp_enqueue_script('marketoForms2', '//app-lon02.marketo.com/js/forms2/js/forms2.min.js');
     }
 }
